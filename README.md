c6d4a039d98c92e8f31630e5037c5bcc708b3f3b
maryletteroa <maryletteroa@gmail.com>
2021-08-07 14:06:47 +0800

    add files

---
+++ README.md
@@ -0,0 +1,14 @@
+#### About
+
+A stub blog site constructed using Flask / Jinja and deployed through Heroku.
+
+#### Link
+
+Visit the page here: 
+
+#### Reference
+
+This is the 59th day exercise for the Udemy course [100 Days of Code - The Python Pro Bootcamp](https://www.udemy.com/course/100-days-of-code/) by Dr. Angela Yu.
+
+My repo for this course is here 👉: [100DaysOfCode-Python](https://github.com/maryletteroa/100DaysOfCode-Python)
+
---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from datetime import datetime
+import requests
+
+# app = Flask(__name__,
+#         static_url_path = "",
+#         static_folder = "static")
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    blog_url = "https://api.npoint.io/e42b353ee387383898c7"
+    response = requests.get(blog_url)
+    posts = response.json()
+    return render_template("index.html", posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/post/<int:id>")
+def get_post(id):
+    blog_url = "https://api.npoint.io/e42b353ee387383898c7"
+    response = requests.get(blog_url)
+    post = response.json()[id-1]
+    return render_template("post.html", post=post)
+
+if __name__ == "__main__":
+    app.run(debug=True)
