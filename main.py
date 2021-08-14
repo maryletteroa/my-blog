732ffd95e61c13658deac6a5da11bd6c9aaaeb45
maryletteroa <maryletteroa@gmail.com>
2021-08-14 22:34:35 +0800

    add edit post, add new post, and delete post, add add sqlite database

--- main.py
+++ main.py
@@ -1,21 +1,81 @@
-from flask import Flask, render_template, request
-import requests
-import smtplib, ssl
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import datetime
 import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['CKEDITOR_PKG_TYPE'] = 'basic'
+ckeditor = CKEditor(app)
+Bootstrap(app)
 
 SMTP = os.environ.get("SMTP")
 MY_EMAIL = os.environ.get("MY_EMAIL")
 PASSWORD = os.environ.get("PASSWORD")
 TO_EMAIL = os.environ.get("TO_EMAIL")
 
-app = Flask(__name__)
+##CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route("/")
-def home():
-    blog_url = "https://api.npoint.io/e42b353ee387383898c7"
-    response = requests.get(blog_url)
-    posts = response.json()
-    return render_template("index.html", posts=posts)
+##CONFIGURE TABLE
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+@app.route('/')
+def get_all_posts():
+    posts = BlogPost.query.all()
+    return render_template("index.html", all_posts=posts)
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = BlogPost.query.get(index)
+    return render_template("post.html", post=requested_post)
+
+@app.route("/edit/<int:post_id>", methods=["GET","POST"])
+def edit_post(post_id):
+    page_title = "Edit Post"
+    post = BlogPost.query.get(post_id)
+    if post:
+        edit_form = CreatePostForm(
+            title=post.title,
+            subtitle=post.subtitle,
+            img_url=post.img_url,
+            author=post.author,
+            body=post.body
+            )
+        if edit_form.validate_on_submit():
+            post.title = edit_form.title.data
+            post.subtitle = edit_form.subtitle.data
+            post.body = edit_form.body.data
+            post.author = edit_form.author.data
+            post.img_url = edit_form.img_url.data
+            db.session.commit()
+            return redirect(url_for("show_post", index=post.id))
+    return render_template("make-post.html", form=edit_form, page_title=page_title)
 
 @app.route("/about")
 def about():
@@ -45,12 +105,30 @@ def contact():
         page_title = "Contact Me"
     return render_template("contact.html", page_title=page_title)
 
-@app.route("/post/<int:id>")
-def get_post(id):
-    blog_url = "https://api.npoint.io/e42b353ee387383898c7"
-    response = requests.get(blog_url)
-    post = response.json()[id-1]
-    return render_template("post.html", post=post)
+@app.route("/new-post", methods=["GET","POST"])
+def new_post():
+    form = CreatePostForm()
+    page_title = "New Post"
+    if form.validate_on_submit():
+        post = BlogPost(title = form.title.data, 
+            subtitle = form.title.data, 
+            date = datetime.now().strftime("%B %d, %Y"),
+            body = form.body.data,
+            author = form.author.data,
+            img_url = form.img_url.data,
+            )
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html", form=form, page_title=page_title)
+
+@app.route("/delete/<int:post_id>")
+def delete(post_id):
+    post = BlogPost.query.get(post_id)
+    if post:
+        db.session.delete(post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
 
 if __name__ == "__main__":
     app.run()
