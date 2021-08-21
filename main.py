584489480c0ed386611fd779426934a53218bff3
maryletteroa <maryletteroa@gmail.com>
2021-08-21 19:26:21 +0800

    uncommet create all

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ class Comment(db.Model):
     post = relationship("BlogPost", back_populates="comments")
     text = db.Column(db.Text, nullable=False)
 
-db.create_all()
+#db.create_all()
 
 
 @login_manager.user_loader
